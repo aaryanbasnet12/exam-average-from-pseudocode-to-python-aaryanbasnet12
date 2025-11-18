7795a87a69c6208868e1b93a638ff872cd5e01fb
Aaryan <samyakbasnet@gmail.com>
2025-11-17 22:19:31 -0600

    Added pseudocode

--- main.py
+++ main.py
@@ -13,6 +13,11 @@ Your pseudocode should describe your overall approach in your own words.
 # Your Python code begins below this line.
 # Every line you write must have a comment directly above it.
 
-# Call your function when your program is ready
-calculate_average()   
-  
+# Print the second score
+print("Second score:", second_score)
+
+# Print the third score
+print("Third score:", third_score)
+
+# Print the average score
+print("The average score is:", average_score)
